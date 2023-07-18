9e99974a1773277c3bf77e5c4f3476ae26ad7331
PiraiKishor <139770556+PiraiKishor@users.noreply.github.com>
2023-07-18 21:11:12 +0530

    Add files via upload

---
+++ ConvertNegToPos.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class ConvertNegToPos {
+
+	public static void main(String[] args) {
+		int num=-40;
+		if(num<0) {
+		System.out.println("The given number is negative");
+		int posnum=num*-1;
+		System.out.println("The converted positive number is" + " "+ posnum );
+	}
+
+}}
+
