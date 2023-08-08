ffce9d5fa07104f99c7606d86e704ac52beb703e
PiraiKishor <139770556+PiraiKishor@users.noreply.github.com>
2023-08-08 17:19:15 +0530

    Add files via upload

---
+++ week4day1/Testngpractice1.java
@@ -0,0 +1,47 @@
+package homeassignments.week4day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Testngpractice1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--disable-notifications");
+
+		ChromeDriver driver = new ChromeDriver(opt);
+		driver.get("https://login.salesforce.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
+		driver.findElement(By.id("password")).sendKeys("Leaf$1234");
+		driver.findElement(By.id("Login")).click();
+
+		driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		WebElement viewbutton = driver.findElement(By.xpath("//button[text()='View All']"));
+		wait.until(ExpectedConditions.elementToBeClickable(viewbutton));
+		viewbutton.click();
+		driver.findElement(By.xpath("//p[text()='Sales']")).click();
+		Thread.sleep(3000);
+//use javascript or action class
+		
+		WebElement opportunity = driver.findElement(By.xpath("(//span[text()='Opportunities'])[1]"));
+		driver.executeScript("arguments[0].click();", opportunity)	;	
+		driver.findElement(By.xpath("//div[text()='New']")).click();
+		//driver.findElement(By.xpath("//input[@name='Name']")).sendKeys("Salesforce automation by LakshmiPriya");
+		String opportunityname = driver.findElement(By.xpath("//input[@name='Name']")).getText();
+		System.out.println(opportunityname);
+		driver.findElement(By.xpath("//input[@name='CloseDate']")).sendKeys("9/8/2023");
+		driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+		String error = driver.findElement(By.xpath("//div[@class='panel-content scrollable']")).getText();
+		System.out.println(error);
+	}
+
+}
